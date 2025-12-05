246843721676fbb3e1f5a16767134105eb771b0c
James M <jmcrafty@gmail.com>
2025-12-05 08:29:56 -0800

    Add files via upload

--- day4part2.c
+++ day4part2.c
@@ -7,40 +7,8 @@
 int grid[GRID_SIZE][GRID_SIZE] = {0};
 int rolls = 0;
 
-void print_grid() {
-    // print the output
-    for (int y = 0; y < GRID_SIZE; y++) {
-        for (int x = 0; x < GRID_SIZE; x++) {
-            if (grid[x][y] >= 4) {
-                printf("@ ");
-            } else if (grid[x][y] == -1) {
-                printf(". ");
-            } else {
-                printf("x ");
-            }
-        }
-        printf("\r\n");
-    }
-}
 
-int clear_grid() {
-    int cleared = 0;
-    for (int y = 0; y < GRID_SIZE; y++) {
-        for (int x = 0; x < GRID_SIZE; x++) {
-            if (grid[x][y] < 4) {
-                if (grid[x][y] != -1) {
-                    grid[x][y] = -1;
-                    rolls++;
-                    cleared = 1;
-                }
-            } else {
-                grid[x][y] = 0;
-            }
-        }
-    }
-    return cleared;
-}
-
+// add counters to each space around the roll
 void space_counter(int x_pos, int y_pos) {
     for (int y = y_pos - 1; y <= y_pos + 1; y++) {
         for (int x = x_pos - 1; x <= x_pos + 1; x++) {
@@ -62,6 +30,28 @@ void space_counter(int x_pos, int y_pos) {
     }
 }
 
+// remove the unblocked paper and reset the counters
+int clear_grid() {
+    // flag
+    int cleared = 0;
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (grid[x][y] < 4) {
+                if (grid[x][y] != -1) {
+                    grid[x][y] = -1;
+                    rolls++;
+                    cleared = 1;
+                }
+            } else {
+                grid[x][y] = 0;
+            }
+        }
+    }
+    return cleared;
+}
+
+
+// open the file to fill the grid while also doing the first parse
 void parse_file() {
     FILE *fp;
     char line[256];
@@ -82,6 +72,7 @@ void parse_file() {
     }
 }
 
+// execute counters on the spaces with paper rolls
 void parse_grid() {
     int x = 0, y = 0;
     for (y = 0; y < GRID_SIZE; y++) {
@@ -90,6 +81,23 @@ void parse_grid() {
                 space_counter(x, y);
             }
         }
+    }
+}
+
+
+void print_grid() {
+    // print the formatted output
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (grid[x][y] >= 4) {
+                printf("@ ");
+            } else if (grid[x][y] == -1) {
+                printf(". ");
+            } else {
+                printf("x ");
+            }
+        }
+        printf("\r\n");
     }
 }
 
